75b238e95cc084c0aacc48079636c0ff959123ca
Graychen <13780185250@sina.cn>
2016-07-18 23:24:09 +0800

    提供composer的锁

---
+++ src/Contracts/RepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Repositories\Contracts;
+interface RepositoryInterface{
+    public function all($columns = array('*'));
+    public function paginate($perPage = array('*'));
+    public function create(array $data);
+    public function update(array $data,$id);
+    public function delete($id);
+    public function find($id,$columns = array('*'));
+    public function findBy($field,$value,$columns=array('*'));
+}
